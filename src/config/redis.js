def2f976be36de6d4f895f00a74fe47b9116810c
Naveen Gowda MY <naveengowda23277@gmail.com>
2025-10-06 09:26:31 +0530

    redis configuration

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,8 +2,7 @@ const redisConnection = {
   username: 'default',
   password: process.env.REDIS_PASSWORD,
   host: process.env.REDIS_HOST,
-  port: parseInt(process.env.REDIS_PORT),
-  maxRetriesPerRequest: null,
+  port: parseInt(process.env.REDIS_PORT) || 13298, maxRetriesPerRequest: null,
   enableReadyCheck: false,
 };
 
